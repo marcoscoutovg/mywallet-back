fcf2d840b94920411bfe1b0e1732d0c37b95621c
Marcos Couto <marcoscouto1997@gmail.com>
2023-04-23 20:11:52 -0300

    ajustes no envio de informações após o login

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -37,11 +37,11 @@ export async function postLogin(req, res) {
     }
 
     try {
-        if (!user) return res.sendStatus(404)
-        if (!bcrypt.compareSync(password, user.password)) return res.sendStatus(401)
+        if (!user) return res.status(404).send("E-mail não encontrado")
+        if (!bcrypt.compareSync(password, user.password)) return res.status(401).send("Senha incorreta")
 
         await db.collection("sessions").insertOne({ userId: user._id, token })
-        res.status(200).send(token)
+        res.status(200).send({name: user.name, token})
     } catch (err) {
         res.status(500).send(err.message);
     }
